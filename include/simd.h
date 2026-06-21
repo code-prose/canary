7d307cb613b6e168751597c5845fe5f4deaf6c10
Logan Rose <logprose@umich.edu>
2026-06-21 19:43:20 -0400

    feat: simd namespace if def guards for type erasure

--- include/simd.h
+++ include/simd.h
@@ -1,47 +1,34 @@
 #pragma once
+#include <cstdint>
+#include <cstddef>
 
 #if defined(__x86_64__) || defined(__amd64__)
     #include <immintrin.h>
-#endif
-
-#if defined(__aarch64__) || defined(__arm__)
+#elif defined(__aarch64__) || defined(__arm__)
     #include <arm_neon.h>
 #endif
 
-// ARM
-#if defined(__ARM_NEON__) || defined (__ARM_NEON)
-#endif
 
-#if defined(__arch64__) || defined(_M_ARM_64) || defined(_M_ARM64EC)
-#endif
-
-#if defined(__ARM_FETATURE_SVE)
-
-#endif
-
-#if defined(__ARM_FETATURE_SVE2)
-
-#endif
-
-
+namespace simd {
 // Linux x85 gcc/clang
-#if defined(__SSE2__) // 16B
-
+#if defined(__AVX512BW__) && defined(__AVX512F__) // 64B - incl byte/word pairs
+    using simd_mask = std::uint64_t;
+    inline constexpr std::size_t kStride = sizeof(__m512i);
+#elif defined(__AVX2__) // 32B
+    using simd_mask = std::uint32_t;
+    inline constexpr std::size_t kStride = sizeof(__m256i);
+#elif defined(__SSE4_2__) // 16B - incl pcmpistri
+    using simd_mask = std::uint16_t;
+    inline constexpr std::size_t kStride = sizeof(__m128i);
+#elif defined(__SSE2__) // 16B
+    using simd_mask = std::uint16_t;
+    inline constexpr std::size_t kStride = sizeof(__m128i);
+#elif defined(__ARM_NEON__) || defined (__ARM_NEON) || \
+        defined(__aarch64__) || defined(_M_ARM64) || defined(_M_ARM64EC) // ARM 64
+    using simd_mask = std::uint64_t;
+    inline constexpr std::size_t kStride = sizeof(uint8x16_t);
+#else
+    using simd_mask = std::unint8_t;
+    inline constexpr std::size_t kStride = 1;
 #endif
-
-#if defined(__SSE4_2__) // 16B - incl pcmpistri
-
-#endif
-
-#if defined(__AVX2__) // 32B
-#endif
-
-#if defined(__AVX512F__) // 64B
-#endif
-
-
-#if defined(__AVX512BW__) // 64B - incl byte/word pairs
-#endif
-
-#if defined(__AVX512VBMI__) // 64B - vpermb/multishift
-#endif
+}

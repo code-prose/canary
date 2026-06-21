6a0217d8393c8bb9fff013b0437b094830ab2036
Logan Rose <logprose@umich.edu>
2026-06-21 18:52:32 -0400

    chore: incomplete scaffolding for parser

---
+++ include/parser.h
@@ -0,0 +1,14 @@
+#include <arm_neon.h>
+#include "simd.h"
+
+namespace Parsing {
+    class FastRejectBase {
+        virtual ConvertSimd();
+        virtual 
+    };
+
+    class SlowPass {
+
+    };
+
+}

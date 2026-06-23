46ea2eed8c1123aaa4fc9c7afec8b8afd12424ff
Logan Rose <logprose@umich.edu>
2026-06-22 20:44:15 -0400

    feat: Parser::run scaffolding

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,26 @@
+#include <string>
+#include <thread>
+
 #include "parser.h"
+#include "mapping.h"
 
 // need different static inits for simd::mask
 //
 //
 // need to check .done() in consumer
+//
+
+// Parser
+Parsing::Report Parsing::Parser::run(const std::string& path) {
+    Mapping m{path};
+    const char* mmap_;
+    Report report{};
+
+    auto producer = []{
+        FastReject frj{};
+    };
+
+    auto consumer = []{
+        SlowPass sp{};
+    };
+}

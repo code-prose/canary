502d6cfee074c87712396bce1b0e331a587b8dfb
Logan Rose <logprose@umich.edu>
2026-06-25 20:45:23 -0400

    feat: T&& overload

--- include/ring_buffer.h
+++ include/ring_buffer.h
@@ -8,6 +8,7 @@ namespace SPSC {
     public:
         RingBuffer() = default;
         bool push(T elem);
+        bool push(T&& elem);
         bool pop(T& out);
         void finish() noexcept { done_.store(true, std::memory_order_release); }
         bool closed() const noexcept { return done_.load(std::memory_order_acquire); }
@@ -22,6 +23,26 @@ namespace SPSC {
 
     template <typename T, std::size_t N>
     bool RingBuffer<T, N>::push(T elem) {
+        std::size_t back, front;
+
+        back = std::atomic_load_explicit(&back_, std::memory_order_relaxed);
+        front = cached_front_;
+        if ((back + 1) % N == front) {
+            cached_front_ = front = std::atomic_load_explicit(&front_, std::memory_order_acquire);
+        }
+        
+        if ((back + 1) % N == front) {
+            return false;
+        }
+
+        data_[back] = elem;
+        // give back_, store the size remaining, enforce memory sequencing
+        std::atomic_store_explicit(&back_, (back + 1) % N, std::memory_order_release);
+        return true;
+    }
+
+    template <typename T, std::size_t N>
+    bool RingBuffer<T, N>::push(T&& elem) {
         std::size_t back, front;
 
         back = std::atomic_load_explicit(&back_, std::memory_order_relaxed);
